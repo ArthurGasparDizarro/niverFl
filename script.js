2d7f3befa0bc816910473bcdaca5f80bfc9c38c4
ArthurGasparDizarro <143722578+ArthurGasparDizarro@users.noreply.github.com>
2024-10-18 16:08:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('birthdayButton').addEventListener('click', function() {
+    document.getElementById('message').classList.remove('hidden'); // Mostra a mensagem ao clicar
+});
